9903b9991625b8f3663575b53a3d18f0fe202022
Alice (KazikovAP) <alice@example.com>
2020-04-02 19:43:03 +0300

    new a and b double

--- main.cpp
+++ main.cpp
@@ -5,13 +5,17 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B:";
-    int a,b;
+    double a,b;
     cin>>a,b;
     cout <<"A+B="<<a+b<<'\n'
     <<"A-B="<<a-b<<'\n'
     <<"A*B="<<a*b<<'\n'
     <<"A/B="<<a/b<<'\n';
+
     if (a>b) cout<<a;
     if (a<b) cout<<b;
+
+    if (b<a) cout<<b;
+    if (b>a) cout<<a;
     return 0;
 }
