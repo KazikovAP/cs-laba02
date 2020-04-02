606a2196ef249b6e128e62705e3cd3f25796c75f
Bob (KazikovAP) <bob@example.com>
2020-04-02 18:52:16 +0300

    search min

--- main.cpp
+++ main.cpp
@@ -11,7 +11,12 @@ int main()
     <<"A-B="<<a-b<<'\n'
     <<"A*B="<<a*b<<'\n'
     <<"A/B="<<a/b<<'\n';
+
     if (a>b) cout<<a;
     if (a<b) cout<<b;
+
+    if (b<a) cout<<b;
+    if (b>a) cout<<a;
+
     return 0;
 }
